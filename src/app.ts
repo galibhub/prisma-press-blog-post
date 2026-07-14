56dfc1cede4325a9e6430c2c09f927e8a378ac92
galibhub <ibrahimgalib00@gmail.com>
2026-07-14 07:39:55 +0600

    feat: implement user registration with profile creation and password hashing

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,86 @@
-import express, { Application, Response,Request } from "express";
-import cors from "cors"
+import express, { Application, Response, Request } from "express";
+import cors from "cors";
 import config from "./config";
 import cookieParser from "cookie-parser";
+import httpStatus from "http-status";
 import { prisma } from "./lib/prisma";
-
-
+import bcrypt from "bcryptjs";
 
 const app: Application = express();
 
 //middleware
-app.use(cors({
-  origin: config.app_url,
-  credentials:true
-}))
+app.use(
+  cors({
+    origin: config.app_url,
+    credentials: true,
+  }),
+);
 
-app.use(express.json())
-app.use(express.urlencoded({extended:true}));
-app.use(cookieParser())
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 // routes
-app.get("/", async(req: Request, res: Response) => {
-  const user = await prisma.user.findMany()
-  console.log(user)
+app.get("/", async (req: Request, res: Response) => {
   res.send("Hello World");
+});
+
+// => Create or Register User
+app.post("/api/users/register", async (req: Request, res: Response) => {
+  const { name, email, password, profilePhoto } = req.body;
+
+  const isUserExist = await prisma.user.findUnique({
+    where: { email },
+  });
+
+  if (isUserExist) {
+    throw new Error("User already exist in this Email");
+  }
+
+  //hash password
+  const hashedPassword = await bcrypt.hash(
+    password,
+    Number(config.bcrypt_salt_rounds),
+  );
+
+  // prisma user create
+  const createdUser = await prisma.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  // create user profile
+  await prisma.profile.create({
+    data: {
+      userId: createdUser.id,
+      profilePhoto,
+    },
+  });
+
+  const user = await prisma.user.findUnique({
+    where: {
+      id: createdUser.id,
+      email: createdUser.email || email,
+    },
+    omit: {
+      password: true,
+    },
+    include: {
+      profile: true,
+    },
+  });
+
+  res.status(httpStatus.CREATED).json({
+    success: true,
+    statusCode: httpStatus.CREATED,
+    message: "User Created Successfully",
+    data: {
+      user,
+    },
+  });
 });
 
 export default app;
