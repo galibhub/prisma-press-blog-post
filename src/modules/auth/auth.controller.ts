bb00df39361cda4bfe3932120d89ac6ccfb66921
galibhub <ibrahimgalib00@gmail.com>
2026-07-15 08:33:53 +0600

    feat(auth): implement JWT login with refresh token stored in HTTP-only cookie

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -2,21 +2,38 @@ import { Request, Response, NextFunction } from "express";
 import { catchAsync } from "../../utils/catchAsync";
 import { authService } from "./auth.service";
 import { sendResponse } from "../../utils/sendResponse";
-import httpStatus  from 'http-status';
+import httpStatus from "http-status";
 
 const loginUser = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
     // login logic here
     const payload = req.body;
-    const loginResult = await authService.loginUser(payload);
+    // const loginResult = await authService.loginUser(payload);
+    const { accessToken, refreshToken } = await authService.loginUser(payload);
 
-    sendResponse(res,{
-        success:true,
-        statusCode:httpStatus.OK,
-        message:"User Logged in successfully",
-        data:loginResult
-    })
-  }
+    //save token into cookie
+    res.cookie("accessToken", accessToken, {
+      httpOnly: true,
+      secure: false,
+      sameSite: "none",
+      maxAge: 1000 * 60 * 60 * 24,
+    });
+
+    res.cookie("refreshToken", refreshToken, {
+      httpOnly: true,
+      secure: false,
+      sameSite: "none",
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+    });
+
+    sendResponse(res, {
+      success: true,
+      statusCode: httpStatus.OK,
+      message: "User Logged in successfully",
+      // data:loginResult
+      data: { accessToken, refreshToken },
+    });
+  },
 );
 
 export const authController = {
